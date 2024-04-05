4c84c6a39aa1dfc357d97783457f323eb2ca3c57
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-04-05 15:45:25 -0300

    feat: created endpoint post: event

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,37 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { z } from "zod";
 
 const app = fastify();
 
-// app.get('/', ()=>{
-//     return 'Hello server funcionando!'
-// })
-
-app.post('/events', (request, reply)=>{
-    console.log(request.body)
-    return 'Hello server funcionando!'
+const prisma = new PrismaClient({
+    log: ['query'],
 })
 
-app.listen({port: 3333}).then(()=>{
+app.post('/events', async (request, reply) => {
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable()
+    })
+
+    const data = createEventSchema.parse(request.body)
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toString(),
+        },
+    })
+
+    return { eventId: event.id }
+})
+
+
+
+
+app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
