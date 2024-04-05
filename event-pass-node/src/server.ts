063a7d1fab80192c675e4c1935f8daf6bbf5f547
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-04-05 00:23:23 -0300

    feat: build test endpoint

--- event-pass-node/src/server.ts
+++ event-pass-node/src/server.ts
@@ -1,4 +1,11 @@
-interface Pessoa{
-    nome: string;
-}
-console.log('Hello Word')
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/', ()=>{
+    return 'Hello server funcionando!'
+})
+
+app.listen({port: 3333}).then(()=>{
+    console.log('HTTP server running!')
+})
