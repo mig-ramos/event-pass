0aeec48693cf9f7c6c2a930ffd7c7c924dc4161b
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-04-05 20:31:16 -0300

    feat: created endpoints: check-in, get-attemdee-badge, get-event-attendees and get-event

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,10 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
 
@@ -10,6 +14,10 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
+app.register(checkIn)
+app.register(getEventAttendees)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
