e2efeaa3a1a4bdcded49a93191f84f44b32efc70
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-04-05 22:41:23 -0300

    feat: install COR of fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,9 @@
 import fastify from "fastify";
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
-import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { ZodTypeProvider, jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
@@ -12,7 +13,11 @@ import { getEventAttendees } from "./routes/get-event-attendees";
 import { errorHandler } from "./error-handler";
 
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 // Documentação
 app.register(fastifySwagger, {
@@ -43,6 +48,6 @@ app.register(getEventAttendees)
 
 app.setErrorHandler(errorHandler)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
     console.log('HTTP server running!')
 })
