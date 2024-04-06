dab859aaf270e7234c6417247af98a2fdaca8ba4
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-04-06 00:18:15 -0300

    feat: create component header

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,12 @@
+import { Header } from "./components/header";
 
 export function App() {
 
-  return <h1 className="text-red-400">Hello Word</h1>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 
